4dcf23e25e8ff51d633338c9c9541ee1e66805b7
IrisWzi <iris.zwz@gmail.com>
2018-04-21 21:57:22 +1000

    Add Ch5 code

---
+++ Chapter5/urllib2_get.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python  
+#coding=utf-8  
+import urllib2  
+  
+url = "http://www.nostarch.com"  
+  
+headers = {}  
+headers['User-Agent'] = "Googlebot"
+  
+request = urllib2.Request(url,headers=headers)  
+response = urllib2.urlopen(request)  
+  
+print(response.read()) 
+response.close()  
